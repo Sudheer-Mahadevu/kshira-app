d6e7debcf486b18383d8a83899fee89ee70638ce
Mahadevu Sudheer <mahadevusudheer274@gmail.com>
2023-06-25 15:38:01 +0530

    setup frontend

---
+++ client/src/pages/index.js
@@ -0,0 +1,13 @@
+import Grid from '@mui/material/Unstable_Grid2';
+
+function LandingPage(){
+    return(
+        <Grid container>
+            <Grid>
+                <p>Hello Material UI</p>
+            </Grid>
+        </Grid>
+    )
+}
+
+export default LandingPage;
